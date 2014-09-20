981dca2f0346c7124be4273ed1e4d81b57f95881
josephmhood <josephmhood@gmail.com>
2014-09-20 00:20:26 -0400

    added good logos

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='PySpyce',
+      version='0.1',
+      description='Python Based SPICE Circuit Simulator',
+      author='Joe Hood',
+      author_email='josephmhood@gmail.net',
+      url='http://josephmhood.github.io/PySpyce/',
+      packages=['pyspyce', 'pyspyce.devices', 'pyspyce.editor'])
